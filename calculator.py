60f08dcbef26b96ccb6ea5f4ab93a94e6e3e2702
NicolasGensollen <nicolas.gensollen@gmail.com>
2020-11-10 10:37:12 +0100

    Add test for multiply and docstrings

--- calculator.py
+++ calculator.py
@@ -4,8 +4,10 @@ Calculator library containing basic math operations.
 
 
 def add(first_term, second_term):
+    """Add first and second term."""
     return first_term + second_term
 
 
 def subtract(first_term, second_term):
+    """Substract second term from first term."""
     return first_term - second_term
