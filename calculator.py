32c91cee90acee443f11bf6219113f443769c0df
NicolasGensollen <nicolas.gensollen@gmail.com>
2020-11-10 10:42:10 +0100

    Add multiply method to calculator and missing docstrings

--- calculator.py
+++ calculator.py
@@ -1,6 +1,4 @@
-"""
-Calculator library containing basic math operations.
-"""
+"""Calculator library containing basic math operations."""
 
 
 def add(first_term, second_term):
@@ -11,3 +9,8 @@ def add(first_term, second_term):
 def subtract(first_term, second_term):
     """Substract second term from first term."""
     return first_term - second_term
+
+
+def multiply(first_term, second_term):
+    """Multiply first term by second term."""
+    return first_term * second_term
--- test_calculator.py
+++ test_calculator.py
@@ -1,17 +1,19 @@
-"""
-Unit tests for the calculator library
-"""
+"""Unit tests for the calculator library"""
 
 import calculator
 
 
 class TestCalculator:
+    """Calss implementing tests for calculator.py."""
 
     def test_addition(self):
+        """Tests for addition."""
         assert 4 == calculator.add(2, 2)
 
     def test_subtraction(self):
+        """Tests for substraction."""
         assert 2 == calculator.subtract(4, 2)
 
     def test_multiplication(self):
+        """Tests for multiplication."""
         assert 100 == calculator.multiply(10, 10)
