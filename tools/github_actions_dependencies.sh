d142f63a56d2624bb11b6d942790aa0cd337750e
NicolasGensollen <nicolas.gensollen@gmail.com>
2020-12-10 10:43:02 +0100

    [skip ci] test

--- tools/github_actions_dependencies.sh
+++ tools/github_actions_dependencies.sh
@@ -9,4 +9,4 @@ else # pip
 	pip uninstall -yq numpy
 	pip install --progress-bar off --upgrade --pre --only-binary ":all:" joblib numpy scipy pandas scikit-learn matplotlib lxml pytest pytest-cov coverage
 fi
-pip install --progress-bar off --upgrade -r requirements_testing.txt
+#pip install --progress-bar off --upgrade -r requirements_testing.txt
