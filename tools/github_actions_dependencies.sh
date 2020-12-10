45932a024579d463d1dcff12e178c67e67947a49
NicolasGensollen <nicolas.gensollen@gmail.com>
2020-12-10 10:20:27 +0100

    conda -> pip for min dep

---
+++ tools/github_actions_dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -ef
+
+if [ ! -z "$CONDA_ENV" ]; then
+	pip uninstall -yq toy_pkg
+elif [ ! -z "$CONDA_DEPENDENCIES" ]; then
+	conda install -y $CONDA_DEPENDENCIES
+else # pip
+	python -m pip install --progress-bar off --upgrade pip setuptools wheel
+	pip uninstall -yq numpy
+	pip install --progress-bar off --upgrade --pre --only-binary ":all:" joblib numpy scipy pandas scikit-learn matplotlib lxml pytest pytest-cov coverage
+fi
+pip install --progress-bar off --upgrade -r requirements_testing.txt
