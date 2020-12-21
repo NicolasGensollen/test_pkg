f01ab217da5169dde18b77498751a4ea2493df5d
NicolasGensollen <nicolas.gensollen@gmail.com>
2020-12-21 13:20:10 +0100

    Try continue on error

--- tools/github_actions_dependencies.sh
+++ tools/github_actions_dependencies.sh
@@ -1,12 +1,9 @@
-#!/bin/bash -e
+#!/bin/bash -x
 
-if [ ! -z "$CONDA_ENV" ]; then
-	pip uninstall -yq toy_pkg
-elif [ ! -z "$CONDA_DEPENDENCIES" ]; then
-	conda install -y $CONDA_DEPENDENCIES
-else # pip
-	python -m pip install --progress-bar off --upgrade pip setuptools wheel
-	pip uninstall -yq numpy
-	pip install --progress-bar off --upgrade --pre --only-binary ":all:" joblib numpy scipy pandas scikit-learn matplotlib lxml pytest pytest-cov coverage
+python -m pip install --progress-bar off --upgrade pip setuptools wheel
+pip install --progress-bar off --upgrade --pre --only-binary ":all:" joblib numpy scipy pandas scikit-learn matplotlib lxml pytest pytest-cov coverage
+
+if [ ! -z "$DEV_VERSION" ]; then
+    pip install git+https://github.com/matplotlib/matplotlib.git
 fi
 #pip install --progress-bar off --upgrade -r requirements_testing.txt
