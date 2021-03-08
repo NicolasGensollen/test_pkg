8851db9f7faaebfee09a588452d554972b96b319
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-03-08 12:05:16 +0100

    Improve setup

--- setup.py
+++ setup.py
@@ -1,22 +1,73 @@
+#! /usr/bin/env python
+
+import os
+import sys
 import setuptools
 
-with open("README.rst", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="toy_pkg",
-    version="0.0.1",
-    author="Nicolas Gensollen",
-    author_email="nicolas.gensollen@gmail.com",
-    description="A small and useless example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/NicolasGensollen/toy_pkg",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+def load_version():
+    """Executes toy_pkg/version.py in a globals dictionary and return it."""
+    globals_dict = {}
+    with open(os.path.join('toy_pkg', 'version.py')) as fp:
+        exec(fp.read(), globals_dict)
+    return globals_dict
+
+
+def is_installing():
+    # Allow command-lines such as "python setup.py build install"
+    install_commands = set(['install', 'develop'])
+    return install_commands.intersection(set(sys.argv))
+
+
+def list_required_packages():
+    required_packages = []
+    required_packages_orig = ['%s>=%s' % (mod, meta['min_version'])
+                              for mod, meta
+                              in _VERSION_GLOBALS['REQUIRED_MODULE_METADATA']
+                              ]
+    for package in required_packages_orig:
+        if package.startswith('sklearn'):
+            package = package.replace('sklearn', 'scikit-learn')
+        required_packages.append(package)
+    return required_packages
+
+# Make sources available using relative paths from this file's directory.
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+_VERSION_GLOBALS = load_version()
+DISTNAME = 'toy_pkg'
+DESCRIPTION = 'A small and useless example package'
+with open('README.rst') as fp:
+    LONG_DESCRIPTION = fp.read()
+MAINTAINER = 'Nicolas Gensollen'
+MAINTAINER_EMAIL = 'nicolas.gensollen@gmail.com'
+URL = 'http://github.com/NicolasGensollen/toy_pkg'
+LICENSE = 'none'
+DOWNLOAD_URL = 'http://github.com/NicolasGensollen/toy_pkg'
+VERSION = _VERSION_GLOBALS['__version__']
+
+if __name__ == "__main__":
+    if is_installing():
+        module_check_fn = _VERSION_GLOBALS['_check_module_dependencies']
+        module_check_fn(is_toy_pkg_installing=True)
+
+    setuptools.setup(
+        name=DISTNAME,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        description=DESCRIPTION,
+        license=LICENSE,
+        url=URL,
+        version=VERSION,
+        download_url=DOWNLOAD_URL,
+        long_description=LONG_DESCRIPTION,
+        zip_safe=False,
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        packages=setuptools.find_packages(),
+        install_requires=list_required_packages(),
+        python_requires='>=3.6',
+    )
---
+++ toy_pkg/version.py
@@ -0,0 +1,83 @@
+# *- encoding: utf-8 -*-
+"""
+toy_pkg version, required package versions, and utilities for checking
+"""
+
+__version__ = '0.0.1'
+
+_NILEARN_INSTALL_MSG = 'See %s for installation information.' % (
+    'http://github.com/NicolasGensollen/toy_pkg')
+
+# This is a tuple to preserve order, so that dependencies are checked
+#   in some meaningful order (more => less 'core').
+REQUIRED_MODULE_METADATA = (
+    ('numpy', {
+        'min_version': '1.11',
+        'required_at_installation': True,
+        'install_info': _NILEARN_INSTALL_MSG}),
+    )
+
+def _import_module_with_version_check(
+        module_name,
+        minimum_version,
+        install_info=None):
+    """Check that module is installed with a recent enough version
+    """
+    from distutils.version import LooseVersion
+
+    try:
+        module = __import__(module_name)
+    except ImportError as exc:
+        user_friendly_info = ('Module "{0}" could not be found. {1}').format(
+            module_name,
+            install_info or 'Please install it properly to use nilearn.')
+        exc.args += (user_friendly_info,)
+        # Necessary for Python 3 because the repr/str of ImportError
+        # objects was changed in Python 3
+        if hasattr(exc, 'msg'):
+            exc.msg += '. ' + user_friendly_info
+        raise
+
+    # Avoid choking on modules with no __version__ attribute
+    module_version = getattr(module, '__version__', '0.0.0')
+
+    version_too_old = (not LooseVersion(module_version) >=
+                       LooseVersion(minimum_version))
+
+    if version_too_old:
+        message = (
+            'A {module_name} version of at least {minimum_version} '
+            'is required to use nilearn. {module_version} was found. '
+            'Please upgrade {module_name}').format(
+                module_name=module_name,
+                minimum_version=minimum_version,
+                module_version=module_version)
+
+        raise ImportError(message)
+
+    return module
+
+
+def _check_module_dependencies(is_toy_pkg_installing=False):
+    """Throw an exception if toy_pkg dependencies are not installed.
+
+    Parameters
+    ----------
+    is_toy_pkg_installing : boolean
+        if True, only error on missing packages that cannot be auto-installed.
+        if False, error on any missing package.
+
+    Throws
+    -------
+    ImportError
+
+    """
+    for (module_name, module_metadata) in REQUIRED_MODULE_METADATA:
+        if not (is_toy_pkg_installing and
+                not module_metadata['required_at_installation']):
+            # Skip check only when installing and it's a module that
+            # will be auto-installed.
+            _import_module_with_version_check(
+                module_name=module_name,
+                minimum_version=module_metadata['min_version'],
+                install_info=module_metadata.get('install_info'))
