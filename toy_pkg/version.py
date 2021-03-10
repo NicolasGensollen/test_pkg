66ca7ff3c196152da4b3790425243903134338b8
Gensollen <nicolas.gensollen@gmail.com>
2021-03-10 10:18:10 +0100

    REL 0.0.3 (#3)

--- toy_pkg/version.py
+++ toy_pkg/version.py
@@ -3,7 +3,7 @@
 toy_pkg version, required package versions, and utilities for checking
 """
 
-__version__ = '0.0.2.dev'
+__version__ = '0.0.3'
 
 _NILEARN_INSTALL_MSG = 'See %s for installation information.' % (
     'http://github.com/NicolasGensollen/toy_pkg')
