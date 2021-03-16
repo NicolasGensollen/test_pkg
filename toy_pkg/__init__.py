f599eceb4c1947f0e4d02d376c73919670db32b9
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-03-16 15:25:18 +0100

    expose version number as __version__

--- toy_pkg/__init__.py
+++ toy_pkg/__init__.py
@@ -5,3 +5,9 @@ Test module in python.
 Documentation is available in the docstrings and online at
 ...
 """
+
+from .version import __version__
+
+# list all submodules available in toy_pkg and version
+__all__ = ['calculator', 'lazy_calculator', 'linalg',
+           '__version__']
