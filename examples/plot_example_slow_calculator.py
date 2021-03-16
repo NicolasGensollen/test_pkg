ca113cce5e63cf0728e0d7e99555898059e23864
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-03-16 16:27:08 +0100

    Rename examples to begin with plot prefix

--- examples/plot_example_slow_calculator.py
+++ examples/plot_example_slow_calculator.py
@@ -13,11 +13,14 @@ from toy_pkg.lazy_calculator import lazy_add, lazy_subtract
 
 #########################################################
 # Perform some relatively long computations
-#
+
 # Perform 1 + 1 and sleep for 10 seconds...
 result = lazy_add(1, 1, 10)
-assert result == 2
+print(result)
 
 # Perform 10 - 7 and sleep for 10 seconds...
 result = lazy_subtract(10, 7, 10)
-assert result == 3
+print(result)
+
+# Perform 20 + 42 and sleeps for 30 seconds...
+print(lazy_add(20, 42, 30))
