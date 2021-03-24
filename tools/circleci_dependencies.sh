cb99587e449f6054f19674dee16f034da9583172
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-03-24 11:09:05 +0100

    debug

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -ef
 
+sudo -E apt-get -yq update
+sudo -E apt-get -yq --no-install-suggests --no-install-recommends --allow-unauthenticated --allow-downgrades --allow-remove-essential --allow-change-held-packages install dvipng
 conda init bash
 echo "conda version = $(conda --version)"
 conda create -n testenv
-conda install -n testenv python=3.8 numpy lxml mkl sphinx numpydoc pillow -yq
-conda install -n testenv sphinx-gallery junit-xml -c conda-forge -yq
+conda install -n testenv -yq python=3.8 numpy scipy scikit-learn matplotlib pandas lxml mkl sphinx numpydoc pillow pandas
+conda install -n testenv -yq nibabel sphinx-gallery junit-xml -c conda-forge
 source activate testenv
 python -m pip install pydata-sphinx-theme
 python -m pip install --user --upgrade --progress-bar off pip setuptools
