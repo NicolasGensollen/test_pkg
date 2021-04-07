b3b555058dabb333a5267556414543851fca20b2
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-04-07 10:37:57 +0200

    Add examples

--- examples/plot_example_dataset_2.py
+++ examples/plot_example_dataset_2.py
@@ -1,16 +1,16 @@
 """
-Example dataset 1
+Example dataset 2
 =================
 
-This example rely on fake dataset 1 only.
+This example rely on fake dataset 2 only.
 """
 
 ######################################################
 # Load dataset
 #
-# Load fake dataset 1
-from toy_pkg.datasets import fetch_fake_dataset_1
-data = fetch_fake_dataset_1()
+# Load fake dataset 2
+from toy_pkg.datasets import fetch_fake_dataset_2
+data = fetch_fake_dataset_2()
 print(data.description)
 
 ######################################################
@@ -19,7 +19,7 @@ import pandas as pd
 from toy_pkg.lazy_calculator import lazy_add
 
 tot = 0
-df = pd.read_csv(data.data)
+df = pd.concat(map(pd.read_csv, data.data))
 for i,row in df.iterrows():
     tot += lazy_add(row['first term'],
                     row['second term'],
