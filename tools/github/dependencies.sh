8f83b69ab63b179a20380c314fe1466b4a9bab11
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-05-20 13:11:24 +0200

    typo

--- tools/github/dependencies.sh
+++ tools/github/dependencies.sh
@@ -4,11 +4,11 @@ python --version
 python -m pip install --upgrade pip setuptools wheel
 pip install joblib numpy scipy pandas scikit-learn matplotlib lxml pytest pytest-cov coverage
 
-if [ ! -z "$DEV_VERSION" ]; then
+if [[ ! -z "$DEV_VERSION" ]]; then
     pip install git+https://github.com/matplotlib/matplotlib.git
 fi
 
-if [ ! -n "$FLAKE8" ]; then
+if [[ -n "$FLAKE8" ]]; then
     echo "Installing Flake8";
     pip install flake8
 fi
