30b037dda8a207134b1fdc96549cb3774815494e
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-05-21 16:45:11 +0200

    update flake8 script

--- tools/flake8_diff.sh
+++ tools/flake8_diff.sh
@@ -30,6 +30,7 @@ echo "TRAVIS EVENT TYPE = $TRAVIS_EVENT_TYPE"
 echo "TRAVIS PULL REQ = $TRAVIS_PULL_REQUEST"
 echo "TRAVIS PULL REQ BRANCH = $TRAVIS_PULL_REQUEST_BRANCH"
 echo "TRAVIS PULL REQ SHA = $TRAVIS_PULL_REQUEST_SHA"
+echo "GITHUB EVENT NAME = $GITHUB_EVENT_NAME"
 
 if [[ "$TRAVIS" == "true" ]]; then
     if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]
@@ -48,12 +49,16 @@ if [[ "$TRAVIS" == "true" ]]; then
         git checkout $BRANCH_NAME
     fi
 elif [[ "$GITHUB_ACTIONS" == "true" ]]; then
-    echo "$GITHUB_SHA"
-    PULL_REQUEST_NUMBER=${GITHUB_REF//*pull\//}
-    PULL_REQUEST_NUMBER=${PULL_REQUEST_NUMBER//\/merge/}
-    BRANCH_NAME=github_pr_$PULL_REQUEST_NUMBER
-    git fetch $REMOTE refs/pull/$PULL_REQUEST_NUMBER/head:$BRANCH_NAME
-    git checkout $BRANCH_NAME
+    if [[ "$GITHUB_EVENT_NAME" == "pull request" ]]; then
+        echo "$GITHUB_SHA"
+        PULL_REQUEST_NUMBER=${GITHUB_REF//*pull\//}
+        PULL_REQUEST_NUMBER=${PULL_REQUEST_NUMBER//\/merge/}
+        BRANCH_NAME=github_pr_$PULL_REQUEST_NUMBER
+        git fetch $REMOTE refs/pull/$PULL_REQUEST_NUMBER/head:$BRANCH_NAME
+        git checkout $BRANCH_NAME
+    else
+        echo "Not PR"
+    fi
 fi
 
 echo -e '\nLast 2 commits:'
