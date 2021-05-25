44d4e7988ef19050a01a88922eb5906e09e4dd7f
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-05-25 12:13:08 +0200

    [arm64] test arm multiple cpu

--- tools/travis/test_script.sh
+++ tools/travis/test_script.sh
@@ -6,10 +6,10 @@ if [[ -n "$FLAKE8" ]]; then
     source tools/flake8_diff.sh
 fi
 
-TEST_CMD="pytest"
+TEST_CMD="pytest --pyargs"
 
 if [[ $TRAVIS_CPU_ARCH == arm64 ]]; then
-    TEST_CMD="$TEST_CMD --pyargs"
+    TEST_CMD="$TEST_CMD --numprocesses=$CPU_COUNT"
 fi
 
 $TEST_CMD toy_pkg
