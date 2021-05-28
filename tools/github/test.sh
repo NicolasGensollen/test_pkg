c5cd420d765e228a452533ebe8a9fe71fc267af6
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-05-28 09:45:00 +0200

    Add doctests

--- tools/github/test.sh
+++ tools/github/test.sh
@@ -4,5 +4,12 @@ if [[ -n "$FLAKE8" ]]; then
     echo "running flake8 diff script...";
     source tools/flake8_diff.sh
 fi
+
 pwd
+
 python -m pytest --pyargs toy_pkg --cov-report=xml --cov=toy_pkg
+
+if [[ $TEST_DOC == true ]]; then
+    echo "Running make test-doc...";
+    make test-doc
+fi
