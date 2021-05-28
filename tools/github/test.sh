725e684997ec2f06a4c6b6a79d1def65f015691f
NicolasGensollen <nicolas.gensollen@gmail.com>
2021-05-28 10:33:15 +0200

    Have flake8 and testdoc as seperate steps

--- tools/github/test.sh
+++ tools/github/test.sh
@@ -1,14 +1,6 @@
 #!/bin/bash -x
 
-if [[ -n "$FLAKE8" ]]; then
-    echo "running flake8 diff script...";
-    source tools/flake8_diff.sh
-fi
-
 pwd
 
 python -m pytest --pyargs toy_pkg --cov-report=xml --cov=toy_pkg
 
-if [[ $TEST_DOC == true ]]; then
-    bash tools/github/test_docs.sh
-fi
